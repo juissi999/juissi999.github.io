3e17651e7057b1d0719ccf42de2ddb60e17f2c12
unknown <jussi.kaatiala@gmail.com>
2020-03-04 13:18:55 +0200

    Slight react element refactoring

--- src/components/elements.js
+++ src/components/elements.js
@@ -37,14 +37,18 @@ const Text = (props) => {
   </p>)
 }
 
-const Ulist = ({elements}) => {
-
+const Txtcombined = ({list}) => {
   const listelement = (e) => e.map((el, i) =>{
     return el
   })
 
+  return (<>{listelement(list)}</>)
+}
+
+const Ulist = ({elements}) => {
+
   const maplist = () => elements.map((el,i) => {
-    return (<li key={i}>{listelement(el)}</li>)
+    return (<li key={i}><Txtcombined list={el}/></li>)
   })
 
   return (<ul>{maplist()}</ul>)
